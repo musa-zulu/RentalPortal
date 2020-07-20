35a9f2f1a4ecaf2ae593dbe7cb0eb390a5ac7ece
Musa <zuluchs@gmail.com>
2020-07-20 17:43:23 +0200

    Initial commit
    added project architecture

---
+++ server/source/RentalPortal.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RentalPortal.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ server/source/RentalPortal.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RentalPortal.Models
+{
+    public class Class1
+    {
+    }
+}
