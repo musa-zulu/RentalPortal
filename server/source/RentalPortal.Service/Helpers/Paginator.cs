c63a846b105e56607d56fe1c45f84fbd2aaeb2bd
Musa <zuluchs@gmail.com>
2020-07-20 21:57:50 +0200

    Added Paginator

---
+++ server/source/RentalPortal.Service/Helpers/Paginator.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentalPortal.Service.Interfaces;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace RentalPortal.Service.Helpers
+{
+    public class Paginator<T> : IPaginator<T> where T : DbSet<T>
+    { 
+        public IQueryable<T> BuildPageResult<TOrder>(
+            IQueryable<T> dbSet,
+            int page,
+            int perPage,
+            Expression<Func<T, TOrder>> orderByExp)
+        {
+            var entsToSkip = (page - 1) * perPage;
+            return dbSet
+                .OrderBy(orderByExp)
+                .Skip(entsToSkip)
+                .Take(perPage);
+        }
+
+        public IQueryable<T> BuildPageResult<TOrder>(
+            IQueryable<T> dbSet, int page, int perPage,
+            Expression<Func<T, bool>> whereExp,
+            Expression<Func<T, TOrder>> orderExp)
+        {
+            var entsToSkip = (page - 1) * perPage;
+
+            return dbSet
+                .OrderBy(orderExp)
+                .Where(whereExp)
+                .Skip(entsToSkip)
+                .Take(perPage);
+        }
+    }
+}
---
+++ server/source/RentalPortal.Service/Interfaces/IPaginator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace RentalPortal.Service.Interfaces
+{
+    public interface IPaginator<T> where T : class
+    {
+        public IQueryable<T> BuildPageResult<TOrder>(
+            IQueryable<T> data,
+            int page,
+            int perPage,
+            Expression<Func<T, TOrder>> orderByExp);
+
+        public IQueryable<T> BuildPageResult<TOrder>(
+            IQueryable<T> data,
+            int page,
+            int perPage,
+            Expression<Func<T, bool>> whereExp,
+            Expression<Func<T, TOrder>> orderByExp
+            );
+    }
+}
